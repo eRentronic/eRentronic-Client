bcebc10cce7e724c498d7e2123eebd2ca1277f86
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-25 16:41:09 +0900

    :lipstick: 공용 컴포넌트 텍스트 컴포넌트 typography 대응을 위한 스타일드 컴포넌트 추가

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -1,4 +1,5 @@
-import { forwardRef, Ref, ElementType } from 'react';
+import { forwardRef, Ref, ElementType, ReactNode } from 'react';
+import styled from 'styled-components';
 
 import { OverRidableProps } from '@/utils/helperType';
 // 추상화 하며 느낀점은 공통으로 들어갈만한 로직을 계속 확장해나가며 중복을 줄인느낌
@@ -6,18 +7,26 @@ type TextBaseProps = {
   typography?: string;
 };
 
-type TextProps<incomeElement extends ElementType> = OverRidableProps<
-  incomeElement,
+type TextProps<incomeElementType extends ElementType> = OverRidableProps<
+  incomeElementType,
   TextBaseProps
 >;
 
 // TODO: 글씨체 관련 코드 작성, 정리
-const Text = <incomeElement extends ElementType = 'span'>(
-  { typography = 'default', ...props }: TextProps<incomeElement>,
+const Text = <incomeElementType extends ElementType = 'span'>(
+  { typography = 'default', as, ...props }: TextProps<incomeElementType>,
   ref: Ref<any>,
 ) => {
-  return <span ref={ref}>{props.children}</span>;
+  const incomeElement = as ?? 'span';
+
+  return (
+    <StyledText ref={ref} as={incomeElement}>
+      {props.children}
+    </StyledText>
+  );
 };
+
+const StyledText = styled.span``;
 
 const ForwardedText = forwardRef(Text);
 
