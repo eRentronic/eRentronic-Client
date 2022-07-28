299f4945db285f8f211ac848acee5a80af01b3e6
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-28 17:10:03 +0900

    :recycle: Text 컴포넌트 타입 스타일 스토리 분리

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -1,16 +1,7 @@
-import { forwardRef, Ref, ElementType, ReactNode } from 'react';
+import { forwardRef, Ref, ElementType } from 'react';
 import styled from 'styled-components';
 
-import { OverRidableProps } from '@/utils/helperType';
-// 추상화 하며 느낀점은 공통으로 들어갈만한 로직을 계속 확장해나가며 중복을 줄인느낌
-type TextBaseProps = {
-  typography?: string;
-};
-
-type TextProps<incomeElementType extends ElementType> = OverRidableProps<
-  incomeElementType,
-  TextBaseProps
->;
+import { TextProps } from './types';
 
 // TODO: 글씨체 관련 코드 작성, 정리
 const Text = <incomeElementType extends ElementType = 'span'>(
