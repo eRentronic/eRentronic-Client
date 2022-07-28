bcc8c21bbcdb8040b2f496df87de9fd47c3df75c
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-07-28 21:33:53 +0900

    :sparkles: 공용 컴포넌트 Input 구현

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -1,9 +1,8 @@
 import { forwardRef, Ref, ElementType } from 'react';
-import styled from 'styled-components';
 
+import { StyledText } from './style';
 import { TextProps } from './types';
 
-// TODO: 글씨체 관련 코드 작성, 정리
 const Text = <incomeElementType extends ElementType = 'span'>(
   { typography = 'default', as, ...props }: TextProps<incomeElementType>,
   ref: Ref<any>,
@@ -16,8 +15,6 @@ const Text = <incomeElementType extends ElementType = 'span'>(
     </StyledText>
   );
 };
-
-const StyledText = styled.span``;
 
 const ForwardedText = forwardRef(Text);
 
