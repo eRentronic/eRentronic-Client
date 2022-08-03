d9b3c4742b5ba3a1cfd1d70d8d04e7135ad4a99d
godhyeongman <aaaa4000@naver.com>
2022-08-03 23:57:36 +0900

    :lipstick: 메인 페이지 메인 태그 레이아웃 개선

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { Header } from '@/components/Header';
+import { Card } from '@/components/Product/Card';
 
 export function Main() {
   return (
@@ -8,8 +9,13 @@ export function Main() {
       <StyledHeader>
         <Header />
       </StyledHeader>
+      <StyledMain>
+        <SearchSection>여기는</SearchSection>
+        <MainContents>
+          <Card />
+        </MainContents>
+      </StyledMain>
       <aside>플러스 사이드</aside>
-      <main>메인</main>
       <footer>푸터</footer>
     </>
   );
@@ -19,4 +25,22 @@ const StyledHeader = styled.header`
   display: flex;
   padding: 20px 0px;
   box-shadow: 0px 11px 22px 5px rgba(0, 0, 0, 0.2);
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+
+  flex-direction: column;
+`;
+
+const SearchSection = styled.section`
+  height: 150px;
+  background-color: aliceblue;
+`;
+
+const MainContents = styled.section`
+  display: flex;
+  width: 60%;
+  margin: 0 auto;
+  flex-wrap: nowrap;
 `;
