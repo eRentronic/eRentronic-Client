836d2fec051932aef81a5bd2013e29a51f546676
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-08-04 16:57:42 +0900

    :bug: Text 컴포넌트 스타일드 컴포넌트 타입 지정 에러 수정 color 가 tyopgraphy 타입으로 지정되어있었음

--- src/components/common/Text/types.ts
+++ src/components/common/Text/types.ts
@@ -2,11 +2,12 @@ import { ElementType } from 'react';
 
 import { globalTheme } from '@/styles/globlaTheme';
 import { OverRidableProps } from '@/utils/helperType';
+
 // 추상화 하며 느낀점은 공통으로 들어갈만한 로직을 계속 확장해나가며 중복을 줄인느낌
 
 export type TextBaseProps = {
   typography?: keyof typeof globalTheme.typography;
-  color?: keyof typeof globalTheme.typography;
+  color?: keyof typeof globalTheme.pallete;
   className?: string;
 };
 
