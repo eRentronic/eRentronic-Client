f37ac84e2c98f2c04d0a25cd1cd212eb8a2df49b
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-08-04 16:36:49 +0900

    :lipstick: 범용 컴포넌트 Text 글로벌Theme 적용하여 스타일 코드 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from '@/styles/globalStyle';
 
 import { App } from './App';
+import { globalTheme } from './styles/globlaTheme';
+
 import '@/styles/index.css';
 
 if (process.env.NODE_ENV === 'development') {
@@ -16,7 +19,9 @@ const root = createRoot(container as Element);
 
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={globalTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
