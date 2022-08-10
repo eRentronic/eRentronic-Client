d8d2187c683dce6efb4a395540bd5a4f45d2ea62
godhyeongman <aaaa4000@naver.com>
2022-08-10 22:38:09 +0900

    :lipstick: 검색창 마크업 및 디자인구현

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { Button, Icon } from '@/components/common';
 import { SideTab } from '@/components/Filter/SideTab/SideTab';
 import { Header } from '@/components/Header';
 import { Card } from '@/components/Product/Card/Card';
@@ -12,47 +13,13 @@ export function Main() {
       </StyledHeader>
       <StyledMain>
         <SearchSection>여기는</SearchSection>
+        <StyledForm>
+          <StyledInput type="search" placeholder="검색어를 입력하세요" />
+          <StyledBtn>
+            <Icon iconSrc="SEARCH" width={25} height={25} />
+          </StyledBtn>
+        </StyledForm>
         <MainContents>
-          <Card
-            title="키보드"
-            thumbnail="https://cdn.inflearn.com/public/courses/328753/cover/0c368e07-0353-4167-a4cb-56726d49218e/%E1%84%8F%E1%85%A5%E1%84%87%E1%85%A5.png"
-            brand="레오폴드"
-            currentPrice={3000}
-            salePrice={2000}
-            discountRate={10}
-            isLike={false}
-            content="제품 설명 테스트 중~"
-          />
-          <Card
-            title="키보드"
-            thumbnail="https://cdn.inflearn.com/public/courses/328753/cover/0c368e07-0353-4167-a4cb-56726d49218e/%E1%84%8F%E1%85%A5%E1%84%87%E1%85%A5.png"
-            brand="레오폴드"
-            currentPrice={3000}
-            salePrice={2000}
-            discountRate={10}
-            isLike={false}
-            content="제품 설명 테스트 중~"
-          />{' '}
-          <Card
-            title="키보드"
-            thumbnail="https://cdn.inflearn.com/public/courses/328753/cover/0c368e07-0353-4167-a4cb-56726d49218e/%E1%84%8F%E1%85%A5%E1%84%87%E1%85%A5.png"
-            brand="레오폴드"
-            currentPrice={3000}
-            salePrice={2000}
-            discountRate={10}
-            isLike={false}
-            content="제품 설명 테스트 중~"
-          />{' '}
-          <Card
-            title="키보드"
-            thumbnail="https://cdn.inflearn.com/public/courses/328753/cover/0c368e07-0353-4167-a4cb-56726d49218e/%E1%84%8F%E1%85%A5%E1%84%87%E1%85%A5.png"
-            brand="레오폴드"
-            currentPrice={3000}
-            salePrice={2000}
-            discountRate={10}
-            isLike={false}
-            content="제품 설명 테스트 중~"
-          />{' '}
           <Card
             title="키보드"
             thumbnail="https://cdn.inflearn.com/public/courses/328753/cover/0c368e07-0353-4167-a4cb-56726d49218e/%E1%84%8F%E1%85%A5%E1%84%87%E1%85%A5.png"
@@ -101,4 +68,37 @@ const MainContents = styled.section`
   width: 60%;
   margin: 0 auto;
   flex-wrap: wrap;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  position: relative;
+  width: 50%;
+  margin: 20px auto;
+`;
+
+const StyledInput = styled.input`
+  border-radius: 9999px;
+  border: none;
+  width: 100%;
+  padding: 10px 20px;
+  font-size: 18px;
+  font-weight: 900;
+  box-shadow: 0px 8px 22px 1px rgba(0, 0, 0, 0.2);
+  :focus {
+    outline: 2px solid ${({ theme }) => theme.pallete.primary};
+  }
+  cursor: pointer;
+`;
+
+const StyledBtn = styled(Button)`
+  position: absolute;
+  border: none;
+  background-color: transparent;
+  width: fit-content;
+  height: fit-content;
+  padding: 10px;
+  right: 5%;
+  top: 5%;
+  cursor: pointer;
 `;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import * as S from './header.styles';
 import { PopUp } from './Popup';
 
 const USER_ICON_LIST: IconTypes[] = ['MENU', 'SHOPPING_CART', 'PERSON'];
+
 const UserIcons = USER_ICON_LIST.map(icon => (
   <Icon iconSrc={icon} key={icon} />
 ));
@@ -13,6 +14,7 @@ const NAV_DATA_LIST: { title: string; options: string[] }[] = [
   { title: '페이지', options: ['홈', '메인'] },
   { title: 'QnA', options: ['고객 안내', '자주 묻는 질문', '1대1 고객상담'] },
 ];
+
 const NAV_CONTENTS = NAV_DATA_LIST.map(({ title, options }) => (
   <PopUp title={title} options={options} />
 ));
