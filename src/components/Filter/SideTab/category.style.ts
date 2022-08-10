ccf022767fa5bf8544602afa960caa3149a08ddb
godhyeongman <aaaa4000@naver.com>
2022-08-10 23:04:45 +0900

    :sparkles: 사이드탭 더보기 기능 구현

--- src/components/Filter/SideTab/category.style.ts
+++ src/components/Filter/SideTab/category.style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { Text } from '@/components/common';
+import { Text, Button } from '@/components/common';
 
 import * as TYPE from './category.type';
 
@@ -16,6 +16,7 @@ export const List = styled.li`
 
 export const CategoryList = styled.ul<TYPE.CategoryListProps>`
   display: ${({ isDisplay }) => (isDisplay ? 'flex' : 'none')};
+  position: relative;
   width: 100%;
   padding: 5px 0;
   flex-direction: row;
@@ -33,4 +34,23 @@ export const CategoryTitle = styled(Text)`
     background-color: ${({ theme }) => theme.pallete.grey2};
     color: ${({ theme }) => theme.pallete.white};
   }
+`;
+
+export const ViewMoreBtn = styled(Button)<TYPE.ViewMoreBtnProps>`
+  display: ${({ isDisplay }) => (!isDisplay ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 50px;
+  height: 20px;
+  background-color: ${({ theme }) => theme.pallete.secondary};
+  border-radius: 9999px;
+  right: 5px;
+  bottom: 5px;
+  border: none;
+  overflow: hidden;
+`;
+
+export const ViewMoreText = styled(Text)`
+  font-size: 1px;
 `;
