56ff9ba15378685f049dacb6c453352ed0ebf9df
godhyeongman <aaaa4000@naver.com>
2022-08-16 13:35:18 +0900

    :sparkles: 헤더 내브 데이터 리스트 팝업 연동을 위한 id 값 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,30 @@
 import { Icon } from '@/components/common';
 import { IconTypes } from '@/components/common/Icon/types';
+import { defaultPopUpOpenState } from '@/recoils/popUp/popUp';
 
 import * as S from './header.styles';
 import { PopUp } from './Popup';
 
 const USER_ICON_LIST: IconTypes[] = ['MENU', 'SHOPPING_CART', 'PERSON'];
 
+const NAV_DATA_LIST: {
+  title: string;
+  options: string[];
+  id: keyof typeof defaultPopUpOpenState;
+}[] = [
+  { title: '페이지', options: ['홈', '메인'], id: 0 },
+  {
+    title: 'QnA',
+    options: ['고객 안내', '자주 묻는 질문', '1대1 고객상담'],
+    id: 1,
+  },
+];
+
 const UserIcons = USER_ICON_LIST.map(icon => (
   <Icon iconSrc={icon} key={icon} />
 ));
-
-const NAV_DATA_LIST: { title: string; options: string[] }[] = [
-  { title: '페이지', options: ['홈', '메인'] },
-  { title: 'QnA', options: ['고객 안내', '자주 묻는 질문', '1대1 고객상담'] },
-];
-
-const NAV_CONTENTS = NAV_DATA_LIST.map(({ title, options }) => (
-  <PopUp title={title} options={options} />
+const NAV_CONTENTS = NAV_DATA_LIST.map(({ title, options, id }) => (
+  <PopUp title={title} options={options} id={id} />
 ));
 
 export function Header() {
