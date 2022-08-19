24d809bf30be95c1a0351467a92fc6100d188f45
godhyeongman <aaaa4000@naver.com>
2022-08-19 14:03:11 +0900

    :recycle: 메인 페이지 스타일 코드 분리

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useResetRecoilState, useRecoilState, useRecoilValue } from 'recoil';
-import styled from 'styled-components';
 
 import * as API from '@/apis/mainProducts';
 import { SideTab } from '@/components/Filter/SideTab/SideTab';
@@ -11,6 +10,8 @@ import { MainInput } from '@/components/Search/MainInput';
 import { popUpOpenState } from '@/recoils/popUp/popUp';
 import { productsStore } from '@/recoils/products/products';
 import { productIds } from '@/recoils/products/selectors/ids';
+
+import * as S from './style';
 
 const getMainProducts = async () => {
   const result = await fetch(`${process.env.MAIN_PRODUCTS}`).then(data =>
@@ -39,51 +40,19 @@ export function Main() {
   }, []);
 
   return (
-    <Wrapper onClick={closeWholePopUp}>
-      <StyledHeader>
+    <S.Wrapper onClick={closeWholePopUp}>
+      <S.StyledHeader>
         <Header />
-      </StyledHeader>
-      <StyledMain>
-        <SearchSection>여기는</SearchSection>
+      </S.StyledHeader>
+      <S.StyledMain>
+        <S.SearchSection>여기는</S.SearchSection>
         <MainInput />
-        <MainContents>{mainContents}</MainContents>
-      </StyledMain>
-      <StyledAside>
+        <S.MainContents>{mainContents}</S.MainContents>
+      </S.StyledMain>
+      <S.StyledAside>
         <SideTab />
-      </StyledAside>
+      </S.StyledAside>
       <footer>푸터</footer>
-    </Wrapper>
+    </S.Wrapper>
   );
 }
-
-const Wrapper = styled.div``;
-
-const StyledHeader = styled.header`
-  display: flex;
-  padding: 20px 0px;
-  box-shadow: 0px 11px 22px 5px rgba(0, 0, 0, 0.2);
-`;
-
-const StyledMain = styled.main`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledAside = styled.aside`
-  position: fixed;
-  width: 15%;
-  left: 5%;
-  top: 30%;
-`;
-
-const SearchSection = styled.section`
-  height: 150px;
-  background-color: aliceblue;
-`;
-
-const MainContents = styled.section`
-  display: flex;
-  width: 60%;
-  margin: 0 auto;
-  flex-wrap: wrap;
-`;
