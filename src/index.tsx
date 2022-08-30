13c60d05f90a3a0bc5a3460bef889fef490415b4
SeungHyun <fm10033@gmail.com>
2022-08-30 20:13:10 +0900

    ♻️ Refact : ThemeProvider 위치 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from '@/styles/globalStyle';
 
 import { App } from './App';
-import { globalTheme } from './styles/globlaTheme';
 import '@/styles/index.css';
 
 if (process.env.NODE_ENV === 'development') {
@@ -26,16 +24,13 @@ const queryClient = new QueryClient({
     },
   },
 });
-
 root.render(
   <React.StrictMode>
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        {/* <ReactQueryDevtools initialIsOpen /> */}
-        <ThemeProvider theme={globalTheme}>
-          <GlobalStyle />
-          <App />
-        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen />
+        <GlobalStyle />
+        <App />
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
