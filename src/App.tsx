74f9fb8a7388a712bd355ce5ba5cae702f2b0524
SeungHyun <fm10033@gmail.com>
2022-08-31 17:55:29 +0900

    ♻️ Refact : 구조변경된 컴포넌트 다크모드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,9 @@ export function App() {
 }
 
 export const StyledHeader = styled.header`
+  width: 100%;
   display: flex;
   padding: 20px 0px;
   box-shadow: 0px 11px 22px 5px rgba(0, 0, 0, 0.2);
+  background: ${({ theme }) => theme.pallete.normalBg};
 `;
