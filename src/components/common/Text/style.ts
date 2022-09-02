8df4ed6b92f1a5e78740c7f38f6c40ad4096bc93
godhyeongman <aaaa4000@naver.com>
2022-09-02 23:45:05 +0900

    :wheelchair: Text 컴포넌트 rest Style 사용 가능하게 변경

--- src/components/common/Text/style.ts
+++ src/components/common/Text/style.ts
@@ -2,10 +2,21 @@ import styled, { css } from 'styled-components';
 
 import { StyledTextProps } from './types';
 
-export const StyledText = styled.span<StyledTextProps>`
-  ${({ theme, color = 'black', typography = 'Regular' }) => css`
-    font-weight: ${theme.typography[typography].weight};
-    font-size: ${theme.typography[typography].size};
-    color: ${theme.pallete[color]};
-  `}
-`;
+export const StyledText = styled.span<StyledTextProps>(
+  ({ theme, color = 'black', typography = 'Regular', styles }) => ({
+    fontWeight: theme.typography[typography].weight,
+    fontSize: theme.typography[typography].size,
+    color: theme.pallete[color],
+    ...styles,
+  }),
+);
+
+// .attrs<StyledTextProps>(({ styles }) => ({
+//   style: styles,
+// }))<StyledTextProps>`
+//   ${({ theme, color = 'black', typography = 'Regular', styles }) => css`
+//     font-weight: ${theme.typography[typography].weight};
+//     font-size: ${theme.typography[typography].size};
+//     color: ${theme.pallete[color]};
+//   `}
+// `;
