67494b16eb0fbcd9f11f681350b89668d231d32e
godhyeongman <aaaa4000@naver.com>
2022-09-20 15:18:42 +0900

    :recycle: 로고 컴포넌트 조건부 네비게이팅 로직 추가

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { Text } from '@/components/common/Text';
 
@@ -8,31 +8,21 @@ const LOGO_SIZE = { small: '40px', large: '50px' };
 const LOGO_MARGIN = { small: '30px', large: '0' };
 
 export function Logo({ size, destination }: LogoProps) {
-  const navigate = useNavigate();
-
   const logoSize = LOGO_SIZE[size];
   const logoMargin = LOGO_MARGIN[size];
 
-  const gotoHome = (e: React.MouseEvent) => {
-    e.preventDefault();
-
-    if (destination) {
-      navigate(destination);
-    }
-  };
-
-  return (
+  const logo = (
     <Text
-      as="a"
       typography="Black"
       styles={{
         fontSize: logoSize,
         marginRight: logoMargin,
         cursor: 'pointer',
       }}
-      onClick={gotoHome}
     >
       eRentronic
     </Text>
   );
+
+  return destination ? <Link to={destination}>{logo}</Link> : logo;
 }
