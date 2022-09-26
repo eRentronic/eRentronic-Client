d474c5d04b98334b557d8caa423f6484c3247a6a
godhyeongman <aaaa4000@naver.com>
2022-09-26 16:21:44 +0900

    :lipstick: 로그인 페이지 레이아웃 및 스타일 개선

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -7,20 +7,34 @@ import { NavText } from '@/components/common/Navigation-text';
 import { UserInput } from '@/components/common/UserInput';
 
 export function LoginPage() {
+  // 로컬스토리지를 통해서 이전 로그인 확인
+
   return (
     <Layout>
       <LoginSection>
-        <Logo size="small" />
-        <UserInput iconSrc="PERSON" placeholder="아이디를 입력하세요" />
-        <UserInput iconSrc="LOCK" placeholder="비밀번호를 입력하세요" />
-        {/* <Caution /> */}
-        <Button>
-          <Text>로그인</Text>
-        </Button>
-        ---
-        <NavText linkPath="/" size="small">
-          회원가입
-        </NavText>
+        <Logo destination="/" size="small" />
+        <Content>
+          <UserInput
+            size="large"
+            iconSrc="PERSON"
+            placeholder="아이디를 입력하세요"
+          />
+          <UserInput
+            size="large"
+            iconSrc="LOCK"
+            placeholder="비밀번호를 입력하세요"
+          />
+          <NavText linkPath="/" size="small">
+            아이디/비밀번호를 잊어버렸나요?
+          </NavText>
+          <Button>
+            <Text>로그인</Text>
+          </Button>
+          <Line />
+          <NavText linkPath="/" size="medium">
+            회원가입
+          </NavText>
+        </Content>
       </LoginSection>
       <Introduce />
     </Layout>
@@ -36,13 +50,25 @@ const Layout = styled.div`
 const LoginSection = styled.section`
   width: 50%;
   padding: 210px 150px;
+  box-sizing: border-box;
+`;
+
+const Content = styled.div`
   display: flex;
   flex-direction: column;
-  box-sizing: border-box;
+  align-items: center;
+  gap: 20px;
+  margin-top: 110px;
 `;
 
 const Introduce = styled.section`
   width: 50%;
   height: 100%;
   background-color: ${({ theme }) => theme.pallete.primary};
+`;
+
+const Line = styled.div`
+  width: 200px;
+  height: 1px;
+  background-color: ${({ theme }) => theme.pallete.grey3};
 `;
--- src/components/common/Caution/index.tsx
+++ src/components/common/Caution/index.tsx
@@ -9,10 +9,13 @@ export function Caution({ content, message }: CautionType) {
     tooShort: `${content}가 너무 짧습니다`,
     tooLong: `${content}가 너무 깁니다`,
   };
+
   const getCautionMessage = (errorType: string) => {
     return cautions[errorType];
   };
+
   const cautionMessage = getCautionMessage(message);
+
   return (
     <Text typography="Light" color="warning" styles={{ fontSize: '8px' }}>
       {cautionMessage}
