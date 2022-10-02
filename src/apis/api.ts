60c7ed03b56d40cf63fc367f6416b669768381e2
godhyeongman <aaaa4000@naver.com>
2022-10-02 20:34:44 +0900

    :recycle: usePostinfo Deprecated -> 커스텀 훅으로 변경됨

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,6 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
-
 // type LoginFoarmType = { name: string; password: string };
 // type SigninFormType = {
 //   name: string;
@@ -14,16 +12,7 @@ import axios from 'axios';
 //   };
 // };
 
-export const usePostInfos = <T>(URL: string, info: T) => {
-  return useMutation(() => postData(URL, info), {
-    onSuccess: data => {
-      console.log(data); // 테스트 통과
-    },
-  });
-};
-
 export const postData = async <T>(URL: string, updatedData: T) => {
   const result = await axios.post(URL, updatedData);
-
   return result;
 };
