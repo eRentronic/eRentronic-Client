90756e2e2a98971ea0581576e065152ea6b8a217
godhyeongman <aaaa4000@naver.com>
2022-10-03 22:09:28 +0900

    :recycle: 로그인/회원가입 비지니스 로직 리팩토링

--- src/service/login.ts
+++ src/service/login.ts
@@ -1,30 +1,63 @@
 import { CautionMessage } from '@/components/common/Caution/types';
 
-const shortValid = (limit: number) => (target: string) =>
+const SHORT_ID_LENGHT = 8;
+const LONG_ID_LENGTH = 20;
+const SHORT_PASSWORD_LENGTH = 4;
+const LONG_PASSWORD_LENGHT = 13;
+
+const validShort = (limit: number) => (target: string) =>
   target.length <= limit ? 'tooShort' : false;
 
-const longValid = (limit: number) => (target: string) =>
+const validLong = (limit: number) => (target: string) =>
   target.length >= limit ? 'tooLong' : false;
 
-const wrongChar = (limitFunc: (value: string) => boolean) => (target: string) =>
-  limitFunc(target) ? false : 'wrongChar';
+const validWrongChar =
+  (limitFunc: (value: string) => boolean) => (target: string) =>
+    limitFunc(target) ? false : 'wrongChar';
+
+const validSamePassword = ({
+  first,
+  second,
+}: {
+  first: string;
+  second: string;
+}) => (first === second ? false : 'wrongPassword');
 
 const getErrorMessages =
-  (...validFuncs: Array<(target: string) => CautionMessage | false>) =>
-  (target: string) => {
+  <T>(...validFuncs: Array<(target: T) => CautionMessage | false>) =>
+  (target: T) => {
     const errorMessages = validFuncs.map(validFunc => validFunc(target));
-
     return errorMessages;
   };
 
-// TODO: 매직 넘버 없애기
-const idShortValid = shortValid(8);
-const idLongValid = longValid(20);
+const idShortValid = validShort(SHORT_ID_LENGHT);
+const idLongValid = validLong(LONG_ID_LENGTH);
+
+const passwordShortValid = validShort(SHORT_PASSWORD_LENGTH);
+const passwordLongValid = validLong(LONG_PASSWORD_LENGHT);
+const passwordReg =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]/;
+const passwordRegValid = passwordReg.test.bind(passwordReg);
+const passwordWrongCharValid = validWrongChar(passwordRegValid);
+
+const nameReg = /^[ㄱ-ㅎ|가-힣|]+$/;
+const nameRegValid = nameReg.test.bind(nameReg);
+
+const nameWrongCharValid = validWrongChar(nameRegValid);
+
 export const idValid = getErrorMessages(idShortValid, idLongValid);
 
-const passwordShortValid = shortValid(4);
-const passwordLongValid = longValid(13);
 export const passwordValid = getErrorMessages(
   passwordShortValid,
   passwordLongValid,
 );
+
+export const signInPasswordValid = getErrorMessages(
+  passwordShortValid,
+  passwordLongValid,
+  passwordWrongCharValid,
+);
+
+export const nameValid = getErrorMessages(nameWrongCharValid);
+
+export const confirmPasswordValid = getErrorMessages(validSamePassword);
