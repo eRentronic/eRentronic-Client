8f1362e6de6fb3251ada578f682b49c19501ed98
godhyeongman <aaaa4000@naver.com>
2022-10-06 22:37:47 +0900

    :recycle: 로그인 폼 템플릿 리팩토링

--- src/templates/LoginForm.tsx
+++ src/templates/LoginForm.tsx
@@ -1,94 +1,152 @@
-import { useState, Dispatch, ChangeEvent } from 'react';
+import { AxiosResponse } from 'axios';
+import {
+  useState,
+  Dispatch,
+  ChangeEvent,
+  ElementType,
+  useReducer,
+} from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { Button, Text } from '@/components/common';
 import { Caution } from '@/components/common/Caution';
-import { CautionMessage } from '@/components/common/Caution/types';
+import {
+  CautionMessage,
+  CautionContent,
+} from '@/components/common/Caution/types';
+import { Logo } from '@/components/common/Logo';
 import { NavText } from '@/components/common/Text/Navigation';
-import { UserInput } from '@/components/common/UserInput';
+import { UserInput, UserInputProps } from '@/components/common/UserInput';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { useMutationPost } from '@/hooks/useMutationPost';
-import { idValid, passwordValid, nameValid } from '@/service/login';
+import { idValid, passwordValid } from '@/service/login';
 
 const onChangeInput =
-  (setterFunc: Dispatch<string>) => (e: ChangeEvent<HTMLInputElement>) => {
+  (dispatch: Dispatch<string>) => (e: ChangeEvent<HTMLInputElement>) => {
     const {
       target: { value },
     } = e;
 
-    setterFunc(value);
+    dispatch(value);
   };
-
-const onToggleInput =
-  (isFocus: boolean, setterFunc: Dispatch<boolean>) => () => {
-    setterFunc(isFocus);
-  };
-
-const checkLoginInfo = (
-  validID: CautionMessage | false,
-  validPassword: CautionMessage | false,
-) => !!(validID && validPassword);
 
 const ID = '아이디';
 const PASSWORD = '비밀번호';
-
 const URL = `${process.env.LOG_IN}`;
 
-export function LoginForm() {
+const DEFAULT_LOGIN_STATE = {
+  id: '',
+  password: '',
+};
+
+type Action = {
+  type: string;
+  payload: string;
+};
+
+const getLoginDispatch =
+  (
+    dispatch: Dispatch<Action>,
+    actionCreator: typeof getIdAction | typeof getPasswordAction,
+  ) =>
+  (value: string) => {
+    const action = actionCreator(value);
+    dispatch(action);
+  };
+
+const getIdAction = (value: string) => ({ type: 'setID', payload: value });
+const getPasswordAction = (value: string) => ({
+  type: 'setPassword',
+  payload: value,
+});
+
+const loginReducer = (state: typeof DEFAULT_LOGIN_STATE, action: Action) => {
+  switch (action.type) {
+    case 'setID':
+      return { ...state, id: action.payload };
+
+    case 'setPassword':
+      return { ...state, password: action.payload };
+
+    case 'reset':
+      return DEFAULT_LOGIN_STATE;
+
+    default:
+      return state;
+  }
+};
+
+type LoginFormProps = {
+  NeedSignInDispatch: () => void;
+};
+
+export function LoginForm({ NeedSignInDispatch }: LoginFormProps) {
   const navigate = useNavigate();
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [isIdBlur, setIsIdBlur] = useState(false);
-  const [isPasswordBlur, setIsPasswordBlur] = useState(false);
-  const { mutate } = useMutationPost(URL, { email: id, password });
+
+  const [loginState, loginDispatch] = useReducer(
+    loginReducer,
+    DEFAULT_LOGIN_STATE,
+  );
+
+  const { getData, setValue } = useLocalStorage('loginState', {
+    isLogin: false,
+    loginToken: '',
+  });
+
+  const { id, password } = loginState;
+
+  const onLoginSuccess = (data: AxiosResponse) => {
+    setValue({ isLogin: true, loginToken: data.headers['access-token'] });
+    navigate('/main');
+  };
+  const onLoginFail = () => {
+    loginDispatch({ type: 'reset', payload: '' });
+  };
+
+  const { mutate } = useMutationPost(
+    URL,
+    { email: id, password },
+    onLoginSuccess,
+    onLoginFail,
+  );
+
+  const idDispatch = getLoginDispatch(loginDispatch, getIdAction);
+  const passwordDipatch = getLoginDispatch(loginDispatch, getPasswordAction);
 
   const idErrorMessages = idValid(id);
   const passwordErrorMessages = passwordValid(password);
 
-  const idValidate = onChangeInput(setId);
-  const onFocusID = onToggleInput(false, setIsIdBlur);
-  const onBlurID = onToggleInput(true, setIsIdBlur);
-
-  const passwordValidate = onChangeInput(setPassword);
-  const onFocusPassword = onToggleInput(false, setIsPasswordBlur);
-  const onBlurPassword = onToggleInput(true, setIsPasswordBlur);
-
-  const idCaution = idErrorMessages.map(
-    idError =>
-      isIdBlur && idError && <Caution content={ID} message={idError} />,
-  );
-
-  const passwordCaution = passwordErrorMessages.map(
-    passwordError =>
-      isPasswordBlur &&
-      passwordError && <Caution content={PASSWORD} message={passwordError} />,
-  );
-
-  // const onSuccessLogin = data => {
-  //   // setterFn(data); useLocalStorage setter함수 사용하면 됨
-  //   navigate('/');
-  // };
+  const idValidate = onChangeInput(idDispatch);
+  const passwordValidate = onChangeInput(passwordDipatch);
 
   return (
     <Content>
-      <UserInput
-        size="large"
-        iconSrc="PERSON"
-        placeholder="아이디를 입력하세요"
-        onChange={idValidate}
-        onBlur={onBlurID}
-        onFocus={onFocusID}
+      <LogoLayout>
+        <Logo size="small" destination="/" />
+      </LogoLayout>
+      <LoginInput
+        inputProps={{
+          size: 'large',
+          iconSrc: 'PERSON',
+          placeholder: '아이디를 입력하세요',
+          onChange: idValidate,
+          value: id,
+        }}
+        cautionContent={ID}
+        inputErrorMessages={idErrorMessages}
       />
-      {idCaution}
-      <UserInput
-        size="large"
-        iconSrc="LOCK"
-        placeholder="비밀번호를 입력하세요"
-        onChange={passwordValidate}
-        onBlur={onBlurPassword}
-        onFocus={onFocusPassword}
+      <LoginInput
+        inputProps={{
+          size: 'large',
+          iconSrc: 'LOCK',
+          placeholder: '비밀번호를 입력하세요',
+          onChange: passwordValidate,
+          value: password,
+        }}
+        cautionContent={PASSWORD}
+        inputErrorMessages={passwordErrorMessages}
       />
-      {passwordCaution}
       <NavText linkPath="/" size="small">
         아이디/비밀번호를 잊어버렸나요?
       </NavText>
@@ -100,10 +158,42 @@ export function LoginForm() {
         <Text>로그인</Text>
       </Button>
       <Line />
-      <NavText linkPath="/" size="medium">
+      <Text styles={{ cursor: 'pointer' }} onClick={NeedSignInDispatch}>
         회원가입
-      </NavText>
+      </Text>
     </Content>
+  );
+}
+
+function LoginInput<incomeElements extends ElementType = 'input'>({
+  inputProps,
+  cautionContent,
+  inputErrorMessages,
+}: {
+  inputProps: UserInputProps<incomeElements>;
+  cautionContent?: CautionContent;
+  inputErrorMessages: (CautionMessage | false)[];
+}) {
+  const [isInputBlur, setIsInputBlur] = useState(false);
+  const onFocusInput = () => {
+    setIsInputBlur(false);
+  };
+
+  const onBlurInput = () => {
+    setIsInputBlur(true);
+  };
+
+  const inputCaution = inputErrorMessages.map(
+    inputError =>
+      isInputBlur &&
+      inputError && <Caution content={cautionContent} message={inputError} />,
+  );
+
+  return (
+    <>
+      <UserInput {...inputProps} onFocus={onFocusInput} onBlur={onBlurInput} />
+      {inputCaution}
+    </>
   );
 }
 
@@ -112,11 +202,16 @@ const Content = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 20px;
-  margin-top: 110px;
 `;
 
 const Line = styled.div`
   width: 200px;
   height: 1px;
   background-color: ${({ theme }) => theme.pallete.grey3};
+`;
+
+const LogoLayout = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 60px;
 `;
