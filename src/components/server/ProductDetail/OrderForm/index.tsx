10dbaf026ad7ad3658ac2dc86a316783d08b12da
SeungHyun <fm10033@gmail.com>
2022-10-27 18:04:51 +0900

    ♻️ : 선택한 재품 기본 정보 컴포넌트 분리

--- src/components/server/ProductDetail/OrderForm/index.tsx
+++ src/components/server/ProductDetail/OrderForm/index.tsx
@@ -15,6 +15,8 @@ import { useMutationPost } from '@/hooks/useMutationPost';
 import { modalStore } from '@/recoils/modal/modal';
 import { stopEventDelivery } from '@/utils/utils';
 
+import { Info } from './Info';
+
 const getInfos = async (path: string) => {
   const result = await axios.get<API.ProductDetail>(path);
   return result.data;
@@ -22,17 +24,19 @@ const getInfos = async (path: string) => {
 
 type DefaultOptionsState = {
   [key: string]: string | number;
-  switch: string;
+  keyboardSwitch: string;
   amount: number;
 };
 
-const defaultOptions: DefaultOptionsState = { switch: '', amount: 1 };
+const defaultOptions: DefaultOptionsState = {
+  keyboardSwitch: '',
+  amount: 1,
+};
 
 type OrderResponseState = {
   id: number;
   message: string;
 };
-
 const defaultOrderResponse: OrderResponseState = {
   id: 0,
   message: '',
@@ -43,8 +47,8 @@ const getValue = (e: ChangeEvent<HTMLInputElement>) => e.target.value;
 const URL = `${process.env.ORDER_PRODUCTS}`;
 
 export function Purchase() {
-  const [isDisplay, setIsDisplay] = useState(false);
-  const [isClicked, setIsClicked] = useRecoilState(modalStore);
+  const [isDisplay, setIsDisplay] = useState(false); // 모달
+  const [isClicked, setIsClicked] = useRecoilState(modalStore); // 모달
   const [orderResponse, setOrderResponse] = useState(defaultOrderResponse);
   const [options, setOptions] = useState(defaultOptions);
   const location = useLocation();
@@ -85,12 +89,26 @@ export function Purchase() {
     throw new Error('데이터 없음');
   }
 
-  const { product, discountInfoResponse } = data;
-  const { id: productId, price: productTotalPrice } = product;
-  const { amount } = options;
-  const { salePrice } = discountInfoResponse;
+  const { product, discountInfoResponse, vendor } = data;
+  const { id: productId, price: productTotalPrice, imageUrl, title } = product;
+  const { keyboardSwitch, amount } = options;
+  const { message } = orderResponse;
+  const { name: brandName } = vendor;
+  const { salePrice, discounts } = discountInfoResponse;
   const totalPrice = salePrice * amount;
+  const salePriceWithComma = salePrice.toLocaleString();
+  const finalPrice = (salePrice * amount).toLocaleString();
   const { address1, address2, zipCode } = address;
+  const ADDRESS = '주소';
+
+  const productInfo = {
+    imageUrl,
+    title,
+    brandName,
+    salePriceWithComma,
+    discounts,
+    productTotalPrice,
+  };
 
   const { mutate } = useMutationPost(
     URL,
@@ -114,17 +132,6 @@ export function Purchase() {
     headers,
   );
 
-  const optionLists = data?.keyboardSwitches.map(({ id, name }) => (
-    <S.Option
-      key={id}
-      onClick={() => {
-        setOptions({ ...options, switch: name });
-      }}
-    >
-      {name}
-    </S.Option>
-  ));
-
   const closeModal = (e: MouseEvent) => {
     e.stopPropagation();
     setIsClicked(!isClicked);
@@ -143,20 +150,60 @@ export function Purchase() {
     }
   };
 
-  const isFormFilled =
-    !Object.keys(address).find(key => !address[key]) &&
-    !Object.keys(options).find(key => !options[key]);
-
   const postOrder = () => {
     mutate();
   };
 
   const setAddressErrorMsg = (inputValue: string) =>
     inputValue.length < 5 ? 'wrongAddress' : '';
-
   const errMessage = setAddressErrorMsg(address.address2);
+  const optionLists = data?.keyboardSwitches.map(({ id, name }) => (
+    <S.Option
+      key={id}
+      onClick={() => {
+        setOptions({ ...options, switch: name });
+      }}
+    >
+      {name}
+    </S.Option>
+  ));
 
-  const content = '주소';
+  const optionList = isDisplay && (
+    <S.OptionList isDisplay={isDisplay}>{optionLists}</S.OptionList>
+  );
+  const chooseAmount = keyboardSwitch && (
+    <S.AmountWrap>
+      <S.PlusBtn onClick={increaseAmount}>+</S.PlusBtn>
+      <Text styles={{ fontSize: '15px' }}>{amount}</Text>
+      <S.MinusBtn onClick={decreaseAmount}>-</S.MinusBtn>
+    </S.AmountWrap>
+  );
+
+  const userInfo = amount && keyboardSwitch && (
+    <S.UserInfo>
+      <S.UserInfoTitle>사용자정보</S.UserInfoTitle>
+      <S.UserInfoContent>
+        주소
+        <S.ChangeAddressBtn type="button" onClick={addressControll}>
+          {address.address1 ? '주소변경' : '주소선택'}
+        </S.ChangeAddressBtn>
+      </S.UserInfoContent>
+      <S.Address1>{address.address1}</S.Address1>
+      <S.Address2
+        placeholder="상세주소를 입력해주세요"
+        onChange={onChangeAddress2}
+      />
+      {errMessage && <Caution content={ADDRESS} message={errMessage} />}
+    </S.UserInfo>
+  );
+
+  const orderResponseModal = !!message && (
+    <S.OrderConfirmation>{message}</S.OrderConfirmation>
+  );
+
+  const isFormFilled =
+    !Object.keys(address).find(key => !address[key]) &&
+    !Object.keys(options).find(key => !options[key]);
 
   return (
     <S.Dimmed isClicked={isClicked} onClick={closeModal}>
@@ -167,51 +214,8 @@ export function Purchase() {
         }}
         isClicked={isClicked}
       >
-        {!!orderResponse.message && (
-          <S.OrderConfirmation>{orderResponse.message}</S.OrderConfirmation>
-        )}
-        <S.InfoWrap>
-          <S.ProductImage src={data.product.imageUrl} />
-          <S.ProductDetail>
-            <S.InfoLeft>
-              <S.ProductInfo>
-                <S.Title>{data.product.title}</S.Title>
-                <S.Brand>{data.vendor.name}</S.Brand>
-              </S.ProductInfo>
-            </S.InfoLeft>
-            <S.InfoRight>
-              <S.PriceCompare>
-                {!!data.discountInfoResponse.discounts.length && (
-                  <S.OriginPrice
-                    styles={{ color: 'grey', fontSize: '10px' }}
-                    forwardedAs="del"
-                  >
-                    {data.product.price.toLocaleString()}
-                  </S.OriginPrice>
-                )}
-                <S.DiscountedPrice>
-                  {`${data.discountInfoResponse.salePrice.toLocaleString()} 원`}
-                </S.DiscountedPrice>
-              </S.PriceCompare>
-
-              {!!data.discountInfoResponse.discounts.length && (
-                <S.DiscountRate color="warning">
-                  {data.discountInfoResponse.discounts.length &&
-                    `${
-                      data.discountInfoResponse.discounts.reduce(
-                        (acc, curr) => {
-                          return {
-                            ...acc,
-                            saleRate: String(+acc.saleRate + +curr.saleRate),
-                          };
-                        },
-                      ).saleRate
-                    }%`}
-                </S.DiscountRate>
-              )}
-            </S.InfoRight>
-          </S.ProductDetail>
-        </S.InfoWrap>
+        {orderResponseModal}
+        <Info info={productInfo} />
         <S.OptionZone>
           <S.DetailOptionBtn
             onClick={e => {
@@ -223,42 +227,13 @@ export function Purchase() {
               option
             </Text>
           </S.DetailOptionBtn>
-          <Text typography="Light">{options.switch}</Text>
-          {isDisplay && (
-            <S.OptionList isDisplay={isDisplay}>{optionLists}</S.OptionList>
-          )}
-          {options.switch && (
-            <S.AmountWrap>
-              <S.PlusBtn onClick={increaseAmount}>+</S.PlusBtn>
-              <Text styles={{ fontSize: '15px' }}>{options.amount}</Text>
-              <S.MinusBtn onClick={decreaseAmount}>-</S.MinusBtn>
-            </S.AmountWrap>
-          )}
+          <Text typography="Light">{keyboardSwitch}</Text>
+          {optionList}
+          {chooseAmount}
         </S.OptionZone>
-
-        {options.amount && options.switch && (
-          <S.UserInfo>
-            <S.UserInfoTitle>사용자정보</S.UserInfoTitle>
-            <S.UserInfoContent>
-              주소
-              <S.ChangeAddressBtn type="button" onClick={addressControll}>
-                {address.address1 ? '주소변경' : '주소선택'}
-              </S.ChangeAddressBtn>
-            </S.UserInfoContent>
-            <S.Address1>{address.address1}</S.Address1>
-            <S.Address2
-              placeholder="상세주소를 입력해주세요"
-              onChange={onChangeAddress2}
-            />
-            {errMessage && <Caution content={content} message={errMessage} />}
-          </S.UserInfo>
-        )}
+        {userInfo}
         <S.PriceAndButton>
-          <S.DiscountedPrice>
-            {`총 ${(
-              data.discountInfoResponse.salePrice * options.amount
-            ).toLocaleString()} 원`}
-          </S.DiscountedPrice>
+          <S.DiscountedPrice>{`총 ${finalPrice} 원`}</S.DiscountedPrice>
           <S.PurchaseButton
             disabled={!isFormFilled}
             isFormFilled={isFormFilled}
