6f9858043da363a3025b010839f5ecb55e5814a7
godhyeongman <aaaa4000@naver.com>
2022-11-07 14:44:53 +0900

    :wrench: 스타일드 컴포넌트 글로벌 테마 및 리액트 라우터 돔 사용가능하게 글로벌 decorator 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,26 @@
+import { ThemeProvider } from 'styled-components';
+
+import { lightMode } from '@/styles/globalTheme';
+import { BrowserRouter } from 'react-router-dom';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  Story => {
+    return (
+      <ThemeProvider theme={lightMode}>
+        <BrowserRouter>
+          <Story></Story>
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+  },
+];
