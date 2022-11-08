3dcc8b6e1955e1cff4fce2c29e0bb48dfd6ec461
SeungHyun <fm10033@gmail.com>
2022-11-08 18:15:48 +0900

    ♻️ 모달 컴포넌트 변경
    props에 radius, children 추가
    useState => useRecoilState로 변경

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,19 +1,17 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, ReactNode } from 'react';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
+import { modalStore } from '@/recoils/modal/modal';
+
 type ModalType = {
-  width: string;
-  height: string;
+  radius?: boolean | string;
+  width?: string;
+  children: ReactNode;
 };
 
-type ModalProps = {
-  modal: ModalType;
-};
-
-export function Modal({ modal }: ModalProps) {
-  const { width, height } = modal;
-  const [isClicked, setIsClicked] = useState(false);
-
+export function Modal({ radius = false, width = '50%', children }: ModalType) {
+  const [isClicked, setIsClicked] = useRecoilState(modalStore);
   const closeModal = () => {
     setIsClicked(!isClicked);
   };
@@ -23,16 +21,25 @@ export function Modal({ modal }: ModalProps) {
     closeModal();
   };
 
+  const onClickModalWrap = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
     <Dimmed isClicked={isClicked} onClick={onClickDimmed}>
-      <ModalWrap isClicked={isClicked} width={width} height={height}>
-        모달
+      <ModalWrap
+        onClick={onClickModalWrap}
+        isClicked={isClicked}
+        radius={radius}
+        width={width}
+      >
+        {children}
       </ModalWrap>
     </Dimmed>
   );
 }
 
-export const Dimmed = styled.div<{ isClicked: boolean }>`
+export const Dimmed = styled.section<{ isClicked: boolean }>`
   display: ${({ isClicked }) => (isClicked ? 'flex' : 'none')};
   position: fixed;
   justify-content: center;
@@ -46,19 +53,18 @@ export const Dimmed = styled.div<{ isClicked: boolean }>`
 
 const ModalWrap = styled.div<{
   isClicked: boolean;
+  radius: boolean | string;
   width: string;
-  height: string;
 }>`
   display: ${({ isClicked }) => (isClicked ? 'flex' : 'none')};
+  width: ${({ width }) => `${width}`};
   position: fixed;
   flex-direction: column;
   align-items: center;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: ${({ width }) => (width ? `${width}` : '50%')};
-  height: ${({ height }) => height};
-  border-radius: 15px;
+  border-radius: ${({ radius }) => (!radius ? 0 : `${radius}`)};
   background: ${({ theme }) => theme.pallete.normalBg};
   padding: 10px;
   gap: 15px;
