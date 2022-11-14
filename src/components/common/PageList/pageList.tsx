0bb585e64e33c472dd1a35095c2cf0d7d1dcbd54
SeungHyun <fm10033@gmail.com>
2022-11-14 18:23:37 +0900

    ✨ :isSkippage에 따른 조건부 랜더링 구현

--- src/components/common/PageList/pageList.tsx
+++ src/components/common/PageList/pageList.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-import { Container } from '@/components/common/Layout/Core';
+import { PageNumber } from './PageNumber';
 
-import { Box } from '../Layout/Box';
-import { Text } from '../Text';
+import { Icon } from '../Icon';
+import { Container } from '../Layout/Core';
 
 type PageListType = {
   state: {
@@ -11,28 +11,52 @@ type PageListType = {
     end: number;
     isSkipPage: boolean;
   };
-  func: {
-    previous: () => void;
-    next: () => void;
-    onClickPage: () => void;
-  };
+  // func: {
+  //   previous: () => void;
+  //   next: () => void;
+  //   onClickPage: () => void;
+  // };
 };
 type PageListProps = {
   pageListProps: PageListType;
 };
-
+const makePageNumberArr = (start: number, end: number) => {
+  const pageNumArr = [];
+  for (let i = start; i <= end; i += 1) {
+    pageNumArr.push(i);
+  }
+  return pageNumArr;
+};
 export function PageList({ pageListProps }: PageListProps) {
   const { state, func } = pageListProps;
   const { start, end, isSkipPage } = state;
-  const { previous, next, onClickPage } = func;
+  // const { previous, next, onClickPage } = func;
+  const mid = Math.floor((start + end) / 2);
+
+  const pageNumberArr = makePageNumberArr(start, end);
+  const pages = !isSkipPage ? (
+    pageNumberArr.map(num => <PageNumber content={num} />)
+  ) : (
+    <>
+      <PageNumber content={start} />
+      <PageNumber content={start + 1} />
+      <PageNumber content="..." />
+      <PageNumber content={mid} />
+      <PageNumber content="..." />
+      <PageNumber content={end - 1} />
+      <PageNumber content={end} />
+    </>
+  );
   return (
-    <Box>
-      <Container>
-        <Text>{start}</Text>
-      </Container>
-      <Container>
-        <Text>{end}</Text>
-      </Container>
-    </Box>
+    <Container
+      backgroundColor="none"
+      justifyContent="center"
+      alignItem="center"
+      gap={10}
+    >
+      <Icon iconSrc="PREV" />
+      {pages}
+      <Icon iconSrc="NEXT" />
+    </Container>
   );
 }
