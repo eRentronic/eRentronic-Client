a24c6317fe8ec4674e605c5034dfc1384025fef4
SeungHyun <fm10033@gmail.com>
2022-11-16 21:08:25 +0900

    ✨ 페이지 포커싱 기능 구현

--- src/components/common/PageList/PageNumber.tsx
+++ src/components/common/PageList/PageNumber.tsx
@@ -3,12 +3,29 @@ import { Text } from '../Text';
 
 type PageNumberProps = {
   content: number | '...';
+  isFocus?: boolean;
 };
 
-export function PageNumber({ content }: PageNumberProps) {
+export function PageNumber({ content, isFocus }: PageNumberProps) {
+  const showingContent = isFocus ? (
+    <Text
+      style={{
+        borderRadius: '999px',
+        backgroundColor: 'black',
+        color: 'white',
+        aspectRatio: '1 / 1',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      {content}
+    </Text>
+  ) : (
+    <Text>{content}</Text>
+  );
   return (
     <Container justifyContent="center" alignItem="center">
-      <Text>{content}</Text>
+      {showingContent}
     </Container>
   );
 }
