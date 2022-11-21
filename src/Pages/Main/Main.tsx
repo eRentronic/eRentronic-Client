2af20b0a00d18a9b91ce4c89cacc458f58610e80
godhyeongman <aaaa4000@naver.com>
2022-11-22 02:30:56 +0900

    :recycle: index 파일 export만 하도록 변경

---
+++ src/Pages/Main/Main.tsx
@@ -0,0 +1,31 @@
+import { Container } from '@/components/common/Layout/Core/Core';
+import { MainInput } from '@/components/server/Search';
+import { useMainProducts } from '@/hooks/useMainProducts';
+import { useProductsFilter } from '@/hooks/useProductsFilter';
+import { getIds } from '@/service/product';
+
+import { Aside } from './Aside';
+import { MainContents } from './Contents';
+import * as S from './style';
+
+export function Main() {
+  const ID = useMainProducts<number[]>({
+    select: data => {
+      const pure = data.content;
+      const ids = getIds(pure);
+      return ids;
+    },
+  })!;
+
+  const filterData = useProductsFilter()!;
+
+  return (
+    <S.Wrapper>
+      <Container flexDirection="column" as="main">
+        <MainInput />
+        <MainContents idList={ID!} />
+      </Container>
+      <Aside {...filterData} />
+    </S.Wrapper>
+  );
+}
