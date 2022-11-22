dcf449641ae828f3d94a5b0f7a4d11a2bf6dc28d
SeungHyun <fm10033@gmail.com>
2022-11-22 16:04:54 +0900

    ✨ stories props dscription 추가

--- src/components/common/PageList/PageList.stories.tsx
+++ src/components/common/PageList/PageList.stories.tsx
@@ -5,6 +5,24 @@ import { PageList } from '@/components/common/PageList/pageList';
 export default {
   component: PageList,
   title: 'View/PageList',
+  argTypes: {
+    start: {
+      description: '페이지 시작점, 기본값 1',
+    },
+    end: {
+      description: '페이지 끝점',
+    },
+    focus: {
+      description: '현제 페이지 번호, 다른 버튼 클릭시 그 번호로 상태 변경',
+    },
+    skipNumber: {
+      description:
+        '생략 기준 번호,리스트 항목 갯수. 이 번호보다 end가 커지면 생략 로직 실행, 기본값 end',
+    },
+    setFocus: {
+      description: 'focus 상태를 바꿔줄 dispatch 함수',
+    },
+  },
 } as ComponentMeta<typeof PageList>;
 
 const Template: ComponentStory<typeof PageList> = args => (
@@ -15,16 +33,4 @@ export const Default = Template.bind({});
 Default.args = {
   end: 10,
   focus: 2,
-};
-export const Skip = Template.bind({});
-Skip.args = {
-  end: 10,
-  focus: 3,
-  skipNumber: 6,
-};
-export const Skip2 = Template.bind({});
-Skip2.args = {
-  end: 5,
-  focus: 3,
-  skipNumber: 6,
 };
