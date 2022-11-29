6d8baa56e2a29c0abc4860d908822010ea5fc4e7
SeungHyun <fm10033@gmail.com>
2022-11-30 00:58:16 +0900

    ✨ 마크업 작업 + 대여시 상태 추가

---
+++ src/components/server/UserInfo/Status/StatusContent.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import { Text } from '@/components/common';
+import { Container } from '@/components/common/Layout/Core';
+
+type StatusContentProps = {
+  text: '입금전' | '배송중' | '배송완료' | '이용중';
+  count: number;
+};
+
+export function StatusContent({ text, count }: StatusContentProps) {
+  const isZero = count === 0;
+  return (
+    <Container
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="10"
+    >
+      <Text>{text}</Text>
+      <StatusNumber isZero={isZero}>{count}</StatusNumber>
+    </Container>
+  );
+}
+
+const StatusNumber = styled.span<{ isZero: boolean }>`
+  color: ${({ theme, isZero }) =>
+    isZero ? theme.pallete.grey3 : theme.pallete.black};
+  font-size: 50px;
+`;
