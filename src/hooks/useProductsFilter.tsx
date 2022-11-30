682cf254ff259b26fda028b45a561ae8046dd1b6
godhyeongman <aaaa4000@naver.com>
2022-11-30 20:48:11 +0900

    :recycle: useProductsFilter Option 파라미터 선택적으로 사용가능하도록 개선

--- src/hooks/useProductsFilter.tsx
+++ src/hooks/useProductsFilter.tsx
@@ -20,15 +20,13 @@ type ProductsFilterProps<T = filterModelType> = UseQueryOptions<
 >;
 
 export function useProductsFilter<formatedDataType = filterModelType>(
-  options: ProductsFilterProps<formatedDataType>,
+  options?: ProductsFilterProps<formatedDataType>,
 ) {
-  // const { select } = options;
-
   const { data, isSuccess } = useQuery<
     filterModelType,
     AxiosError,
     formatedDataType
-  >(['productFilterQueryKey'], getProductsFilter);
+  >(['productFilterQueryKey'], getProductsFilter, options);
 
   if (isSuccess) {
     return data;
